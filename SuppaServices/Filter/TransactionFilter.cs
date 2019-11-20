c829012ed728deb85bc032eb3466b5946b01500c
Ian Johnson <ipjohnson123@yahoo.com>
2019-11-20 07:56:47 -0500

    updating nuget packages and adding example transaction filter

---
+++ SuppaServices/Filter/TransactionFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyRpc.AspNetCore;
+
+namespace SuppaServices.Server.Filter
+{
+    public class TransactionFilter : ICallExecuteFilter, ICallExceptionFilter
+    {
+        /// <summary>
+        /// Filters participate in dependency injection so you can take request scoped dependencies
+        /// </summary>
+        public TransactionFilter()
+        {
+
+        }
+
+        public async Task BeforeExecute(ICallExecutionContext context)
+        {
+            // open transaction
+        }
+
+        public async Task AfterExecute(ICallExecutionContext context)
+        {
+            // commit transaction
+        }
+
+        public async Task HandleException(ICallExecutionContext context, Exception exception)
+        {
+            // rollback transaction
+        }
+    }
+}
--- SuppaServices/Startup.cs
+++ SuppaServices/Startup.cs
@@ -1,9 +1,11 @@
+using System.Transactions;
 using EasyRpc.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuppaServices.Server.Filter;
 using SuppaServices.Server.Services;
 
 namespace SuppaServices.Server
@@ -20,23 +22,17 @@ namespace SuppaServices.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app.UseJsonRpc("/",
+                api =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    api.ApplyFilter<TransactionFilter>();
+                    api.ExposeAssemblyContaining<Startup>().Where(TypesThat.AreInTheSameNamespaceAs<BitmapService>());
                 });
-            });
-
-            app.UseJsonRpc("",
-                api => api.ExposeAssemblyContaining<Startup>().Where(TypesThat.AreInTheSameNamespaceAs<BitmapService>()));
         }
     }
 }
