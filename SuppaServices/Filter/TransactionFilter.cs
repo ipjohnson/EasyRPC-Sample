84c57c096e766cc7c75d1539fcfa001041426433
Ian Johnson <ipjohnson123@yahoo.com>
2019-12-02 07:14:16 -0500

    adding transaction logic

--- SuppaServices/Filter/TransactionFilter.cs
+++ SuppaServices/Filter/TransactionFilter.cs
@@ -3,32 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EasyRpc.AspNetCore;
+using SuppaServices.Server.DataAccess;
 
 namespace SuppaServices.Server.Filter
 {
     public class TransactionFilter : ICallExecuteFilter, ICallExceptionFilter
     {
+        private readonly IConnectionManager _connectionManager;
+
         /// <summary>
         /// Filters participate in dependency injection so you can take request scoped dependencies
         /// </summary>
-        public TransactionFilter()
+        public TransactionFilter(IConnectionManager connectionManager)
         {
-
+            _connectionManager = connectionManager;
         }
 
         public async Task BeforeExecute(ICallExecutionContext context)
         {
-            // open transaction
+            _connectionManager.BeginTransaction();
         }
 
         public async Task AfterExecute(ICallExecutionContext context)
         {
-            // commit transaction
+            _connectionManager.CommitTransaction();
         }
 
         public async Task HandleException(ICallExecutionContext context, Exception exception)
         {
-            // rollback transaction
+            _connectionManager.RollbackTransaction();
         }
     }
 }
